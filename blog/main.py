235f6b49c4ad2bcceb6dae3b5e36f64260cee8bd
Deepthi Purijala <saideepthi.p@veltris.com>
2024-10-15 10:30:37 +0530

    response model, user creation and password hashing

--- blog/main.py
+++ blog/main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI, Depends, HTTPException, status, Response
-from . import schemas
-from . import models
+from . import schemas, models
 from .database import engine, SessionLocal
 from typing import Annotated
 from sqlalchemy.orm import Session
+from .hashing import Hash
 
 app = FastAPI()
 
@@ -29,20 +29,21 @@ def createBlog(blog: schemas.Blog, db: db_dependency):
     return {'message': 'Blog created successfully'}
 
 # get all blogs 
-@app.get("/blogs")
+# we are getting a list of blogs where each blog is of ShowBLog type response body
+@app.get("/blogs", response_model=list[schemas.ShowBlog])
 def getBlogs(db: db_dependency):
     blogs = db.query(models.Blog).all()
-    return {"blogs": blogs}
+    return blogs
 
 # get blog by id
-@app.get("/blog/{blog_id}", status_code=status.HTTP_200_OK)
+@app.get("/blog/{blog_id}", status_code=status.HTTP_200_OK, response_model=schemas.ShowBlog)
 def getBlogById(blog_id:int, response: Response, db:db_dependency):
     blog = db.query(models.Blog).filter(models.Blog.id == blog_id).first()
     if not blog:
         response.status_code=status.HTTP_404_NOT_FOUND
         return {'detail': f'blog with id: {blog_id} is not found'}
     else:
-        return {"blog": blog}
+        return blog
     
 # delete a blog by id
 @app.delete("/blog/{blog_id}", status_code=status.HTTP_200_OK)
@@ -59,6 +60,7 @@ def deleteBlogById(blog_id:int, response: Response, db:db_dependency):
 # update a blog by id
 @app.put("/blog/{blog_id}", status_code=status.HTTP_200_OK)
 def updateBlog(blog_id:int, request: schemas.Blog, response:Response, db:db_dependency):
+    # db.query(models.Blog).filter(models.Blog.id == blog_id).update(request)
     # Update the blog entry
     update_query = db.query(models.Blog).filter(models.Blog.id == blog_id)
     
@@ -75,3 +77,24 @@ def updateBlog(blog_id:int, request: schemas.Blog, response:Response, db:db_depe
     db.commit()
     
     return {'detail': 'Blog updated successfully'}
+
+
+# create a user
+@app.post('/user')
+def createUser(request: schemas.User,  db:db_dependency):
+    new_user = models.User( name=request.name,
+    email=request.email,
+    password=Hash.bcrypt(request.password)
+    )
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return {'detail': 'User created successfully', 'user': new_user}
+
+# get user by id
+@app.get("/user/{user_id}", response_model=schemas.ShowUser)
+def getUser(user_id: int, db:db_dependency):
+    user = db.query(models.User).filter(models.User.id == user_id).first()
+    if not user:
+        raise HTTPException(status_code=404, detail='User not found with given id')
+    return user
